7f736c66e20cc00612595e4163fcfa5fc377a1da
Bijay <yamku202@gmail.com>
2019-09-09 23:25:00 +1000

    Update dynamo_utils.py
    added unknown model for dynamo-db

--- doorman/dynamo_utils.py
+++ doorman/dynamo_utils.py
@@ -42,3 +42,40 @@ def update_dynamo(username, userid, match_percentage, image_id, url):
         image_url = url
     )
     put_into_dynamo.save()
+
+class RekognitionUnKnown(Model):
+    """
+    Defines the Rekognition model for UnKnown People's DynamoDB
+    """
+
+    class Meta:
+        table_name = 'deeplens-unknown-faces'
+        region = session.Session().region_name
+        read_capacity_units = 5
+        write_capacity_units = 5
+    
+    sydtz = pytz.timezone('Australia/Sydney')
+    time_in_sydney = datetime.now(sydtz)
+    time_now = str(time_in_sydney.strftime('%Y-%m-%d %H:%M:%S'))
+    
+    #user_name = UnicodeAttribute(hash_key=True)
+    #slack_user_id = UnicodeAttribute(range_key=True)
+    #match_percentage = NumberAttribute(null=False)
+    image_id = UnicodeAttribute(null=False)
+    captured_at = UnicodeAttribute(default=time_now)
+    image_url = UnicodeAttribute(null=False)
+    # sentiment analysis = ???
+    
+def update_dynamo(match_percentage, image_id, url):
+    """
+    This function updates unknown-faces dynamo table
+    """
+    
+    put_into_dynamo = RekognitionUnKnown(
+        ##user_name = username,
+        slack_user_id = userid,
+        match_percentage = round(match_percentage),
+        image_id = image_id,
+        image_url = url
+    )
+    put_into_dynamo.save()
